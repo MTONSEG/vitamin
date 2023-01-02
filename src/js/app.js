7fc066941fb0d6588ceb579864013a72588c3a6b
Maksym Puzanov <puzanov.maksym@gmail.com>
2023-01-02 14:00:04 +0200

    add choose and comments sections in the  home page

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,38 @@
 import { isWebp } from "./modules/functions.js";
 import * as dynamicAdaptive from './modules/dynamicAdapt.js';
-import * as select from './modules/select.js';
-import * as burger from './modules/burgerMenu.js';
-import * as cartPopup from './modules/cartPopup.js';
-import * as lazyMap from './modules/lazyMap.js';
+// import * as select from './modules/select.js';
+// import * as burger from './modules/burgerMenu.js';
+// import * as cartPopup from './modules/cartPopup.js';
+// import * as lazyMap from './modules/lazyMap.js';
+import Swiper from 'swiper';
 
 isWebp();
 
-
-// Форма колличества товаров
-const quantityBody = document.querySelectorAll('.quantity');
-
-if (quantityBody) {
-	for (let elem of quantityBody) {
-		let input = elem.querySelector('.quantity__input');
-
-		elem.addEventListener('click', e => {
-			let onClickMinus = e.target.closest('._minus');
-			let onClickPlus = e.target.closest('._plus');
-
-			if (onClickMinus) {
-				input.value--;
-			} else if (onClickPlus) {
-				input.value++;
-			}
-
-			if (input.value <= 1) {
-				input.value = 1;
-			}
-		})
+const chooseSlider = new Swiper('.slider-choose', {
+	slidesPerView: 'auto',
+	spaceBetween: 10,
+	breakpoints: {
+		768: {
+			slidesPerView: 2,
+		},
+		1220: {
+			slidesPerView: 2,
+			spaceBetween: 15
+		},
+		1440: {
+			slidesPerView: 2,
+			spaceBetween: 33
+		} 
 	}
-}
+})
+
+const commentsSlider = new Swiper('.slider-comments', {
+	slidesPerView: 'auto',
+	spaceBetween: 10,
+	breakpoints: {
+		1221: {
+			slidesPerView: 3,
+			spaceBetween: 32
+		}
+	}
+})

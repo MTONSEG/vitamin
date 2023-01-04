620bb918ff9c499a075fce7c34b67ebfb2b57068
Maksym Puzanov <puzanov.maksym@gmail.com>
2023-01-04 23:31:03 +0200

    add order block to the profile page

--- src/js/components/forms.js
+++ src/js/components/forms.js
@@ -1,5 +1,5 @@
 //Импортирую ф-ии добавления класса
-import { addClass, removeClass} from "../modules/functions.js";
+import { addClass, removeClass, toggleClass } from "../modules/functions.js";
 
 //Меню учетной записи
 const profile = document.querySelector('.profile');
@@ -9,16 +9,12 @@ if (profile) {
 	const itemBody = profile.querySelectorAll('.profile__content');
 	const body = document.querySelector('body');
 
-	console.log(itemBody);
-
 	itemLink.forEach(elem => {
 		elem.addEventListener('click', e => {
 			let currentLink = e.target.closest('.profile__link');
 			let linkId = currentLink.getAttribute('data-link');
 			let currentBody = document.querySelector(linkId);
 			let activeItem = currentLink.querySelector('_active');
-
-			console.log(currentLink)
 
 			if (!activeItem) {
 				for (let item of itemLink) {
@@ -37,3 +33,55 @@ if (profile) {
 	})
 }
 
+// const accordion = document.querySelectorAll('.item-order');
+
+// if (accordion) {
+// 	for (let elem of accordion) {
+// 		let accordionBtn = elem.querySelector('.accordion-header');
+// 		let accordionBody = elem.querySelector('.accordion-body');
+// 		let bodyHeight = accordionBody.scrollHeight + 'px';
+
+// 		accordionBtn.addEventListener('click', e => {
+
+// 			if (accordionBody.style.maxHeight) {
+// 				accordionBody.style.maxHeight = 0;
+// 			} else {
+// 				accordionBody.style.maxHeight = 0;
+// 				accordionBody.style.maxHeight = bodyHeight;
+// 			}
+
+// 		})
+// 	}
+// }
+
+
+// Аккордеон
+const accordion = document.querySelector('.accordion');
+
+if (accordion) {
+	const headersAccordion = accordion.querySelectorAll('.accordion-header');
+	const bodiesAccordion = accordion.querySelectorAll('.accordion-body')
+
+	for (let elem of headersAccordion) {
+		elem.addEventListener('click', () => {
+
+			let content = elem.nextElementSibling;
+			let icon = elem.querySelector('.item-order__icon');
+
+			if (content.style.maxHeight) {
+				for (let elem of bodiesAccordion) {
+					elem.style.maxHeight = null;
+					removeClass(icon, '_active');
+				}
+			} else {
+				for (let elem of bodiesAccordion) {
+					elem.style.maxHeight = null;
+					removeClass(icon, '_active');
+
+					content.style.maxHeight = content.scrollHeight + 'px';
+					addClass(icon, '_active');
+				}
+			}
+		})
+	}
+}

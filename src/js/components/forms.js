d2724e4500146ac8f73106ab41ae44614ae5c266
Maksym Puzanov <puzanov.maksym@gmail.com>
2023-01-07 14:22:33 +0200

    finish product page

--- src/js/components/forms.js
+++ src/js/components/forms.js
@@ -108,3 +108,15 @@ if (totalHeader) {
 		}
 	})
 }
+
+//Изменение текста Autoship в карточке товара
+
+const autoshipText = document.querySelector('.body-product__autoship p');
+
+if (autoshipText) {
+	let displayWidth = window.innerWidth;
+
+	if (displayWidth < 992) {
+		autoshipText.innerHTML = 'Deliver every';
+	}
+}

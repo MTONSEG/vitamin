b140aebbad71fa69e8e565331209a875c5f26528
Maksym Puzanov <puzanov.maksym@gmail.com>
2023-01-10 15:21:19 +0200

    remove import quiz.js in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import { isWebp, removeClass, addClass } from "./modules/functions.js";
 // import * as dynamicAdaptive from './modules/dynamicAdapt.js';
 import * as select from './modules/select.js';
 import * as burger from './modules/menu.js';
-import * as quiz from './components/quiz.js'
+// import * as quiz from './components/quiz.js';
 // import * as forms from './components/forms.js';
 // import * as cartPopup from './modules/cartPopup.js';
 import * as quantity from './modules/quantity.js';
